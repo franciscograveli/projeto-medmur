ff03b3f04797e61599fbb4eff924d492497a0029
Francisco Graveli <fcdasj@hotmail.com>
2024-02-10 11:16:48 -0300

    inital commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+echo 'projeto-medmur';
+// require __DIR__."/vendor/autoload.php";
+
+// use CoffeeCode\Router\Router;
+
+// $router = new Router(URL_BASE);
+
+// /*
+//  * Controllers
+//  */
+// $router->namespace("Source\App");
+
+// /*
+//  * WEB
+//  * home
+//  */
+// $router->group(null);
+// $router->get("/", "Web:home");
+// $router->get("/{filter}", "Web:home");
+
+// /*
+//  * Blog 
+//  */
+// $router->group("blog");
+// $router->get("/", "Web:blog");
+// $router->get("/{post_uri}", "Web:post");
+// $router->get("/{categoria}/{cat_uri}", "Web:category");
+
+
+// /*
+//  * Contato
+// */
+// $router->group("contato");
+// $router->get("/" , "Web:contact");
+// $router->get("/{sector}" , "Web:contact");
+// $router->get("/suporte" , "Web:support");
+
+// /*
+//  * ADMIN
+//  * Home
+// */
+// $router->group("admin");
+// $router->get("/", "Admin:home");
+
+
+// /*
+//  * ERROS
+//  */
+// $router->group("ooops");
+// $router->get("/{errcode}", "Web:error");
+
+
+// $router->dispatch();
+
+// if($router->error()) {
+//     $router->redirect("/ooops/{$router->error()}");
+// }
